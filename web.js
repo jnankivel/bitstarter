d8b1ca7b3c0411f7e02a96909418e92bda26d7fe
jnankivel <josh@nankivel.com>
2013-07-07 11:39:29 -0700

    Update web.js
    updated to write index.html to buffer and then write it out as a string.

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express'), fs=require("fs");
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
